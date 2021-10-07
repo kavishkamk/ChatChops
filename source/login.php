e9893e69497b6e8fbdada4abe5ec5d9134054511
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2021-10-07 15:25:50 +0530

    footer stylesheet added

--- source/login.php
+++ source/login.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Logging</title>
         <link rel="stylesheet" type="text/css" href="css/login.css">
+        <link rel="stylesheet" type="text/css" href="css/footer.css">
     </head>
     <body>
         <div class=container>
