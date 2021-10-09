d853f555684d1b4cf7b386e870ae33f488d644b9
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2021-10-09 17:10:45 +0530

    footer added, form action updated (but errors)

--- source/google-login/logged-in.php
+++ source/google-login/logged-in.php
@@ -1,11 +1,8 @@
 <?php
-//require_once 'glogin-config.php';
+require_once 'glogin-config.php';
 
-//user data
-$fname = "Rashmi";
-$lname = "Wijesekara";
-$email;
-$pic;
+$fname = $_SESSION['fname'];
+$lname = $_SESSION['lname'];
 
 ?>
 <html>
@@ -13,21 +10,40 @@ $pic;
         <title>Password Setting</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../css/glogin-password.css">
+        <link rel="stylesheet" href="../css/footer.css">
     </head>
     <body>
     <center>
         <div class='logo'><img src= "../images/chatchops.png"></div>
         
-        <div class = "user-name-display">
+        <div class = "username-display">
         <b><?php echo $fname . " ". $lname; ?></b></div>
 
         <div class = "welcome">
             Welcome to ChatChops!
         </div>
 
+        <form class= "pswd-setting" action="../include/google-signin.inc.php" method="post">
+        <div class="form-header">
+            <h1>Password Setting</h1>
+        </div>
         <div id= "box" style= "position: center;">
-        <form class= "pswd-setting" action="include/register.inc.php" method="post">
+            <b><label for="pswd">Password</label></b><br>
+            <input type="password" name="pswd" placeholder="Enter password" size="30" class="pswd-enter">
+            <br><br>
+            <b><label for="pswd">Confirm Password</label></b><br>
+            <input type="password" name="confirm-pswd" placeholder="Enter password again" size="30" class="pswd-enter">
+            <br>
+                    
+            <button type="submit" name="pswd-submit" class="logbutn">Enter</button>
+        </div>
+        </form>
     </center>
+
+    <footer style= "margin-top: 180px;">
+        <p>Copyright &copy; 2021 ChatChops. Inc. All rights reserved</p>
+    </footer>
+
     </body>
 </html>
 
