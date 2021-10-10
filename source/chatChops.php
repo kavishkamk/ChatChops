336802ee26a8ce755c8d0eed50fe5eea19d4876b
kavishkamk <kavishkamadhushan727@gmail.com>
2021-10-10 11:42:11 +0530

    solved few interface issues

--- source/chatChops.php
+++ source/chatChops.php
@@ -3,9 +3,16 @@
 ?>
         <!-- This is a chat interface -->
         <main>
-
-
-        
+            <div class = "chatContainer" id="chat">
+                <div class="chat" id="private-chat" style="grid-column:1 / 2; grid-row: 1 / 3">
+                </div>
+                <div class="chat" id="chat-gui" style="grid-column:2 / 3; grid-row: 1 / 3">
+                </div>
+                <div class="chat" id="private-group" style="grid-column:3 / 4; grid-row: 1 / 2">
+                </div>
+                <div class="chat" id="public-group" style="grid-column:3 / 4; grid-row: 2 / 3">
+                </div>
+            </div>
         </main>
 
     </body>
