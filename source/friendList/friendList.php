ff20bbdb5e50ae005c5a61432b01d099285cec3a
kavishkamk <kavishkamadhushan727@gmail.com>
2021-10-13 01:46:46 +0530

    added and modified friend List UI

--- source/friendList/friendList.php
+++ source/friendList/friendList.php
@@ -3,8 +3,26 @@
 ?>
         <!-- This is a chat interface -->
         <main>
-            <div>
-                
+            <div class="friend-list">
+                <div id="frinds" class="friend-container" style="grid-column:2 / 3; grid-row: 1 / 4">
+                    <div class="search-bar">
+                    </div>
+                    <div class="friend-accounts">
+                        <div class="friend-conversation active">
+                            <img src="../profile-pic/unknownPerson.jpg"/>
+                            <div class="title-text">
+                                Kavishka Madhushan
+                            </div>
+                            <div class="request-button">
+                                <button type=submit name="requset-submit" class="request-btn">ADD</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="friend-request" class="friend-container" style="grid-column:4 / 5; grid-row: 1 / 2">
+                </div>
+                <div id="send-request" class="friend-container" style="grid-column:4 / 5; grid-row: 3 / 4">
+                </div>
             </div>
         </main>
 
