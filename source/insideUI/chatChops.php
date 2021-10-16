124479abbcb6579bdffea55b2c8e4d2cd36b7b7b
kavishkamk <kavishkamadhushan727@gmail.com>
2021-10-16 14:05:57 +0530

    added simple interface for chat window

--- source/insideUI/chatChops.php
+++ source/insideUI/chatChops.php
@@ -46,6 +46,37 @@
                     </div>
                 </div>
                 <div class="chat" id="chat-gui" style="grid-column:2 / 3; grid-row: 1 / 3">
+                    <div class="chat-title">
+                        <span>Kavishka Madhushan</span>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                    <div class="chat-body">
+                        <div class="chat-message-list">
+                            <div class="message-row your-message">
+                                <div class="message-content">
+                                    <div class="message-text">Ok then</div>
+                                    <div class="message-time"></div>
+                                </div>
+                            </div>
+                            <div class="message-row other-message">
+                                <div class="message-content">
+                                    <img src="../profile-pic/unknownPerson.jpg"/>
+                                    <div class="message-text">Ok ok</div>
+                                    <div class="message-time"></div>
+                                </div>
+                            </div>
+                            <div class="message-row your-message">
+                                <div class="message-content">
+                                    <div class="message-text">Ok bro</div>
+                                    <div class="message-time"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat-form">
+                            <input type="text" placeholder="type a message"/>
+                            <button type="submit" class="send-msg"><i class='fas fa-paper-plane'></i></button>
+                        </div>
+                    </div>
                 </div>
                 <div class="chat" id="private-group" style="grid-column:3 / 4; grid-row: 1 / 2">
                 </div>
