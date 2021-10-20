84baa16ae36a7900a7255bed055fa8c52642ac41
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2021-10-20 15:17:54 +0530

    chat rooms display fixed

--- source/insideUI/chatChops.php
+++ source/insideUI/chatChops.php
@@ -1,7 +1,7 @@
 
     <!-- This is a chat interface -->
     <head>
-    <link rel="stylesheet" type="text/css" href="../css/chatUI.css">
+    
 
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     <style>
@@ -10,7 +10,7 @@
     }
     </style>
 
-    
+    <link rel="stylesheet" type="text/css" href="../css/chatUI.css">
     </head>
     <body>
         <main>
@@ -22,32 +22,43 @@
                 <div class="chat" id="private-chat" style="grid-column:1 / 2; grid-row: 1 / 3">
                 </div>
                 <div class="chat" id="chat-gui" style="grid-column:2 / 3; grid-row: 1 / 3">
-                
                 </div>
+
                 <div class="chat" id="private-group" style="grid-column:3 / 4; grid-row: 1 / 2">
                     <div class= "topic">
                         Private Groups
-                        <hr>
                     </div>
                 </div>
+                
                 <div class="chat" id="public-group" style="grid-column:3 / 4; grid-row: 2 / 3">
-                    <table>
-                        <th>
-                            <div class= "topic">
-                                Public Chat Rooms
-                            </div>
-                        </th>
-                        <th>
-                            <a href="../public-rooms/create-pub-room.php">
-                            <button id= "create-room-button">Create Room</button>
-                            </a>
-                        </th>
-                    </table>
-                    <hr>
-                    
-
-                    <div class= "pub rooms">
+                    <div class= "topic">
+                        Public Chat Rooms
                     </div>
+
+                    <div style= "position: right">
+                        <a href="../public-rooms/create-pub-room.php">
+                        <button id= "create-room-button">Create Room</button>
+                        </a>
+                    </div>
+
+                    <div class= "pub-room-list">
+                        <?php
+                            //take #of rooms active
+                            //while loop count = #of rooms active
+                            //create div class= room for each room details (ref = chat msg in w3schools)
+                            // <a href> to each div to load room into chat ui in the middle
+                        ?>
+
+                        <!--sample rooms display-->
+                        <div class= "room">
+                            Room 1
+                        </div>
+
+                        <div class= "room">
+                            Room 2
+                        </div>
+                    </div>
+                
                 </div>
             </div>
         </main>
