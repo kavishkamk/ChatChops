a5eaa86a6b661b964aff4c8d2b7a6514d63bdd79
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2021-11-02 18:48:25 +0530

    chat rooms list modified

--- source/insideUI/chatChops.php
+++ source/insideUI/chatChops.php
@@ -10,7 +10,9 @@
     }
     </style>
 
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
     <link rel="stylesheet" type="text/css" href="../css/chatUI.css">
+
     </head>
     <body>
         <main>
@@ -22,11 +24,24 @@
                 <div class="chat" id="private-chat" style="grid-column:1 / 2; grid-row: 1 / 3">
                 </div>
                 <div class="chat" id="chat-gui" style="grid-column:2 / 3; grid-row: 1 / 3">
+                
+                    <div class="chat-title">
+                        chat title
+                        <span id="reserver-name"> group name</span>
+                    </div>
+                    <div class="chat-body">chat body</div>
+                
                 </div>
 
                 <div class="chat" id="private-group" style="grid-column:3 / 4; grid-row: 1 / 2">
                     <div class= "topic">
                         Private Groups
+                    </div>
+
+                    <div style= "position: right">
+                        <a href="#">
+                        <button id= "grp">Create Group</button>
+                        </a>
                     </div>
                 </div>
                 
@@ -37,26 +52,48 @@
 
                     <div style= "position: right">
                         <a href="../public-rooms/create-pub-room.php">
-                        <button id= "create-room-button">Create Room</button>
+                        <button id= "rm">Create Room</button>
                         </a>
                     </div>
 
-                    <div class= "pub-room-list">
+                    <div class= "pub-room-list" style="min-width: 400px; max-height: 225px; overflow-x: visible; overflow-y: scroll;">
                         <?php
-                            //take #of rooms active
-                            //while loop count = #of rooms active
-                            //create div class= room for each room details (ref = chat msg in w3schools)
+                            
                             // <a href> to each div to load room into chat ui in the middle
+
+                        include_once "../public-rooms/displayRoomList.class.php";
+                        
+                        $roomObj = new displayRoomList();
+                        $count = $roomObj->roomCount();
+                        
+                        $roomname;
+                        
+                        if($count == 0){
+                            echo '<div class= "room">No chat rooms</div>';
+                        }
+                        else if($count == "sqlerror"){
+                            echo '<script>alert ("Something went wrong");</script>';
+                        }
+                        else{
+                            $arr = $roomObj-> fullRoomSetData();
+                            for($i=0; $i<$count; $i++){
+                                $roomname = $arr[$i]['name'];
+                                $icon = $arr[$i]['icon'];
+                                $memCount = $roomObj-> getMemberCount($roomname);
+
+                                echo "<a href= '#' class= 'groupChat-load' style='text-decoration:none;'><div class= 'room'>
+                                <img src= '../group-icons/$icon' alt='group icon' width='35'height='35' class='img-circle pro-img'>&emsp; 
+                                $roomname
+                                <span class= 'memcount' style='float: right'>$memCount Members</span>
+                                </div></a>";
+                                
+                            }
+
+                            
+
+                        }
+                        unset($roomObj);
                         ?>
-
-                        <!--sample rooms display-->
-                        <div class= "room">
-                            Room 1
-                        </div>
-
-                        <div class= "room">
-                            Room 2
-                        </div>
                     </div>
                 
                 </div>
@@ -68,7 +105,7 @@
         $msg = "";
         if(isset($_GET['status'])){
             $msg = setMessage();
-            echo '<script>alert ("'.$msg.'")</script>';
+            echo '<script>alert ("'.$msg.'")</>';
         }
         
         function setMessage()
