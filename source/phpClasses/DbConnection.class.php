3e673a0a84ad2d71c331154accc33115f8c5a2f9
Kavishka Madhushan <70341750+kavishkamk@users.noreply.github.com>
2022-01-04 12:38:43 +0530

    Db connection Details added

--- source/phpClasses/DbConnection.class.php
+++ source/phpClasses/DbConnection.class.php
@@ -2,10 +2,10 @@
     // this class for get databse connection
     class DbConnection {
 
-        private $dbservername = "localhost:3307";
-        private $dBuserName = "root";
-        private $dBpassword = "";
-        private $dBname = "chatchops";
+        private $dbservername = "remotemysql.com:3306";
+        private $dBuserName = "qi68lgkqMS";
+        private $dBpassword = "U5ahDqIXu6";
+        private $dBname = "qi68lgkqMS";
 
         protected function connect(){
             $conn = mysqli_connect($this->dbservername, $this->dBuserName, $this->dBpassword, $this->dBname);
