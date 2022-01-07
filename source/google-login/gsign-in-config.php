3798cba7921ddcc39add02e3781d2107a567f36f
räshmï <89580298+Rashmi-Wijesekara@users.noreply.github.com>
2022-01-07 15:49:37 +0530

    Update gsign-in-config.php

--- source/google-login/gsign-in-config.php
+++ source/google-login/gsign-in-config.php
@@ -5,7 +5,7 @@
 $login_button = "";
 
 //root = /htdocs/
-require $_SERVER['DOCUMENT_ROOT'].'/source/GoogleAuth/vendor/autoload.php'; 
+require 'https://chatchops.herokuapp.com/source/GoogleAuth/vendor/autoload.php'; 
 
 $clientID = '98629541381-h09adtlbp8nru45h09if0i8b7m9l3qd0.apps.googleusercontent.com';
 $clientSecret = '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';
